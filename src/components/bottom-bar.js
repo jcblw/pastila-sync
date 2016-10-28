267d669160c30ffdffb2e6e321598209fb6d7d2a
jcblw <jacoblowe2.0@gmail.com>
2016-10-28 12:16:00 -0700

    add in sync toggle on click

--- src/components/bottom-bar.js
+++ src/components/bottom-bar.js
@@ -4,12 +4,16 @@ import {title} from '../styles/fonts'
 import {colorTundora} from '../styles/colors'
 
 export default ({
-  gistSyncing
+  gistSyncing,
+  onSubmit
 }) => (
   <div>
     <h5
       {...title}
       {...colorTundora}
+      onClick={(e) => onSubmit(e, {
+        'gist-syncing': !gistSyncing
+      })}
     >
       Syncing is {gistSyncing ? 'on' : 'off'}
     </h5>
